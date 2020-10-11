0beb1dc4c32c38b2f7f0ed95d09036385338341c
Gustavo Oliveira <gustavo.gcdo@gmail.com>
2020-10-11 17:40:34 -0400

    improving ux for mobile

--- src/shared/components/PaginationComponent/Pagination.tsx
+++ src/shared/components/PaginationComponent/Pagination.tsx
@@ -9,21 +9,33 @@ type PaginateProps = {
   onChangePage?: (page: number) => void;
 };
 
-const Pagination: FunctionComponent<PaginateProps> = props => {
-  const pagerInit = getPager(props.totalItems, props.currentPage, props.pageSize, props.maxPages);
+const Pagination: FunctionComponent<PaginateProps> = (props) => {
+  const pagerInit = getPager(
+    props.totalItems,
+    props.currentPage,
+    props.pageSize,
+    props.maxPages
+  );
   const [pager, setPager] = useState(pagerInit);
 
-  useEffect(
-    () => {
-      const pagerInit = getPager(props.totalItems, props.currentPage, props.pageSize, props.maxPages);
-      setPager(pagerInit);
-    },
-    [props.totalItems, props.pageSize, props.maxPages, props.currentPage]
-  );
+  useEffect(() => {
+    const pagerInit = getPager(
+      props.totalItems,
+      props.currentPage,
+      props.pageSize,
+      props.maxPages
+    );
+    setPager(pagerInit);
+  }, [props.totalItems, props.pageSize, props.maxPages, props.currentPage]);
 
   function setPage(page: number) {
     if (page < 1 || page > pager.totalPages) return;
-    const newPager = getPager(props.totalItems, page, props.pageSize, props.maxPages);
+    const newPager = getPager(
+      props.totalItems,
+      page,
+      props.pageSize,
+      props.maxPages
+    );
     setPager(newPager);
 
     if (props.onChangePage) {
@@ -31,7 +43,12 @@ const Pagination: FunctionComponent<PaginateProps> = props => {
     }
   }
 
-  function getPager(totalItems: number = 0, currentPage: number = 0, pageSize: number = 10, maxPages: number = 8) {
+  function getPager(
+    totalItems: number = 0,
+    currentPage: number = 0,
+    pageSize: number = 10,
+    maxPages: number = 5
+  ) {
     const totalPages = Math.ceil(totalItems / pageSize);
 
     if (currentPage < 1) {
@@ -60,7 +77,9 @@ const Pagination: FunctionComponent<PaginateProps> = props => {
       }
     }
 
-    const pages = Array.from(Array(endPage + 1 - startPage).keys()).map(i => startPage + i);
+    const pages = Array.from(Array(endPage + 1 - startPage).keys()).map(
+      (i) => startPage + i
+    );
 
     return {
       totalItems: totalItems,
@@ -69,44 +88,58 @@ const Pagination: FunctionComponent<PaginateProps> = props => {
       totalPages: totalPages,
       startPage: startPage,
       endPage: endPage,
-      pages: pages
+      pages: pages,
     };
   }
 
+  const isFirstPage = () => {
+    return pager.currentPage === 1 || pager.currentPage === 0;
+  };
+
+  const isLastPage = () => {
+    return pager.currentPage === pager.totalPages;
+  };
+
   return (
-    <ul className="pagination">
-      <li className="totalItems">
-        Total of {props.totalItems} records
-      </li>
-      <li className={pager.currentPage === 1 || pager.currentPage === 0 ? 'disabled' : ''} onClick={() => setPage(1)}>
-        <span>First</span>
-      </li>
-      <li
-        className={pager.currentPage === 1 || pager.currentPage === 0 ? 'disabled' : ''}
-        onClick={() => setPage(pager.currentPage - 1)}
-      >
-        <span>Previous</span>
-      </li>
-      {pager.pages.map((page, index) =>
-        <li key={index} className={pager.currentPage === page ? 'active' : ''} onClick={() => setPage(page)}>
-          <span>
-            {page}
-          </span>
+    <>
+      <ul className="pagination">
+        <li className="totalItems">Total of {props.totalItems} records</li>
+        <li className="currentPage">Current page: <span className="numPage">{pager.currentPage}</span></li>
+        <li
+          className={`action ${isFirstPage() && 'disabled'}`}
+          onClick={() => setPage(1)}
+        >
+          <span>First</span>
         </li>
-      )}
-      <li
-        className={pager.currentPage === pager.totalPages ? 'disabled' : ''}
-        onClick={() => setPage(pager.currentPage + 1)}
-      >
-        <span>Next</span>
-      </li>
-      <li
-        className={pager.currentPage === pager.totalPages ? 'disabled' : ''}
-        onClick={() => setPage(pager.totalPages)}
-      >
-        <span>Last</span>
-      </li>
-    </ul>
+        <li
+          className={`action ${isFirstPage() && 'disabled'}`}
+          onClick={() => setPage(pager.currentPage - 1)}
+        >
+          <span>Previous</span>
+        </li>
+        {pager.pages.map((page, index) => (
+          <li
+            key={index}
+            className={pager.currentPage === page ? 'active' : ''}
+            onClick={() => setPage(page)}
+          >
+            <span>{page}</span>
+          </li>
+        ))}
+        <li
+          className={`action ${isLastPage() && 'disabled'}`}
+          onClick={() => setPage(pager.currentPage + 1)}
+        >
+          <span>Next</span>
+        </li>
+        <li
+          className={`action ${isLastPage() && 'disabled'}`}
+          onClick={() => setPage(pager.totalPages)}
+        >
+          <span>Last</span>
+        </li>
+      </ul>
+    </>
   );
 };
 
